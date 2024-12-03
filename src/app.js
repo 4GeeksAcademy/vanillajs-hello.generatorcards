50880dabf46def52f1ed7212751e43f47323a978
Anabel VM <anabelvivasm@gmail.com>
2024-12-03 18:13:48 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,32 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let palos = ["♦", "♥", "♠", "♣"]; // palos
+  let numeros = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ]; //numeros
+
+  // Función para obtener un valor aleatorio de un array
+
+  let paloRandom = palos[Math.floor(Math.random() * palos.length)];
+  let numeroRandom = numeros[Math.floor(Math.random() * numeros.length)];
+
+  let palo1 = document.getElementById("palo1");
+  palo1.innerText = paloRandom;
+
+  let numero = document.getElementById("numero");
+  numero.innerText = numeroRandom;
+
+  let palo2 = document.getElementById("palo2");
+  palo2.innerText = paloRandom;
 };
